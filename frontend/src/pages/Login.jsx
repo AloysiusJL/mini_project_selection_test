7039b04941c516147d52954b345d56fc6b508467
Aloysius Jeremy Laksono <aloysius.laksono@gmail.com>
2023-05-13 16:49:28 +0700

    add login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
@@ -9,24 +10,55 @@ import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link as RouterLink } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
-
-const validationSchema = Yup.object().shape({
-  email: Yup.string().required('Email is required').email('Invalid email address'),
-  password: Yup.string().required('Password is required'),
-});
+import Axios from 'axios';
+import Swal from 'sweetalert2';
 
 const theme = createTheme();
 
 export default function Login() {
-  const handleSubmit = (values) => {
-    console.log(values);
-  };
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = async (values, { setSubmitting }) => {
+    try {
+      setSubmitting(true);
+  
+      const { email, password } = values;
+      console.log('Email or Username:', email);
+      console.log('Password:', password);
+  
+      const response = await Axios.post('http://localhost:8000/login', {
+        usernameOrEmail: email,
+        password: password
+      });
+  
+      console.log('Login Response:', response.data);
+  
+      // Perform any desired actions after successful login
+  
+      Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: 'Logged in successfully',
+      });
+    } catch (err) {
+      console.error('Error logging in:', err);
+  
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Invalid credentials. Please try again.',
+      });
+    } finally {
+      setSubmitting(false);
+    }
+  };  
 
   return (
     <ThemeProvider theme={theme}>
@@ -51,59 +83,75 @@ export default function Login() {
               email: '',
               password: '',
             }}
-            validationSchema={validationSchema}
             onSubmit={handleSubmit}
           >
-            <Form noValidate>
-              <Field
-                as={TextField}
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                label="Username or Email"
-                name="email"
-                autoComplete="email"
-                autoFocus
-              />
-              <ErrorMessage name="email" component="div" />
-              <Field
-                as={TextField}
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <ErrorMessage name="password" component="div" />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
-              />
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-              >
-                Sign In
-              </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link component={RouterLink} to="/forgot-password" variant="body2">
-                    Forgot password?
-                  </Link>
+            {({ isSubmitting, setFieldValue, values }) => (
+              <Form noValidate>
+                <Field
+                  as={TextField}
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Username or Email"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <Field
+                  as={TextField}
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type={showPassword ? 'text' : 'password'}
+                  id="password"
+                  autoComplete="current-password"
+                  InputProps={{
+                    endAdornment: (
+                      <Button
+                        onClick={() => setShowPassword(!showPassword)}
+                        onMouseDown={(e) => e.preventDefault()}
+                        edge="end"
+                        aria-label="toggle password visibility"
+                      >
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </Button>
+                    ),
+                  }}
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? (
+                    <CircularProgress size={24} color="inherit" />
+                  ) : (
+                    'Sign In'
+                  )}
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link component={RouterLink} to="/forgot-password" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link component={RouterLink} to="/register" variant="body2">
+                      Don't have an account? Sign up
+                    </Link>
+                  </Grid>
                 </Grid>
-                <Grid item>
-                  <Link component={RouterLink} to="/register" variant="body2">
-                    Don't have an account? Sign up
-                  </Link>
-                </Grid>
-              </Grid>
-            </Form>
+              </Form>
+            )}
           </Formik>
         </Box>
         <Box mt={5}>
